3321b95714526f57e3adf8650ee09a6af8a90fd1
Jack Benton <jtb.benton@gmail.com>
2025-08-29 12:31:02 -0400

    Integrate Note model with Flask app and initialize database

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from models import database, Note  
 
 app = Flask(__name__)
+
+# Initialize the database
+with app.app_context():
+    database.create_tables([Note], safe=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
