0ad1ce0fe97eee53205658eea123cbbe99585d19
Jack Benton <jtb.benton@gmail.com>
2025-08-29 12:33:32 -0400

    Register API endpoints with Flask app

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
-from models import database, Note  
+from models import database, Note
+from resources import create_note, get_notes
 
 app = Flask(__name__)
 
 # Initialize the database
 with app.app_context():
     database.create_tables([Note], safe=True)
+
+# Register endpoints
+app.route('/notes', methods=['GET'])(get_notes)
+app.route('/notes', methods=['POST'])(create_note)
 
 if __name__ == "__main__":
     app.run(debug=True)
